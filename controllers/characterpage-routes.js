08eacaf1009f708f88c25e1f0746427c373985a9
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-02-05 10:57:01 -0600

    stuff

--- controllers/characterpage-routes.js
+++ controllers/characterpage-routes.js
@@ -1,9 +1,35 @@
+const { Character, User } = require("../models");
 const router = require("express").Router();
 const withAuth = require("../utils/auth");
 
 //Protected the character route by forcing there to be a user that is logged in to see the page.
 router.get("/character", withAuth, (req, res) => {
-  res.render("characterpage");
+  Character.findAll({
+    //Character is the Model Name and .findAll is the method so we get
+    //all character data from the Character table.
+    where: {
+      user_id: req.session.user_id,
+    },
+    include: [
+      //Including the username from the User model as well since there is a
+      //Foreign key on the Character Model (user_id) that can match up w/ the username
+      //via the users table (id).
+      {
+        model: User,
+        attributes: ["id", "username"],
+      },
+    ],
+  }).then((dbCharData) => {
+    //creating a variable and calling it characters which is mapping over the
+    //data in the character table.
+    const characters = dbCharData.map((character) =>
+      character.get({ plain: true })
+    );
+    console.log("character page existing characters", characters);
+    //once all this data is pulled we render the scores page and include the characters data
+    //as an object so it can be used w/ our handlebars template.
+    res.render("characterpage", { characters });
+  });
 });
 
 module.exports = router;
--- public/javascript/create-character.js
+++ public/javascript/create-character.js
@@ -21,7 +21,34 @@ async function newFormHandler(event) {
     alert("Character Names Must be unique!");
   }
 }
+async function anotherFormHandler(event) {
+  event.preventDefault();
+
+  const char_name = document.querySelector(
+    'select[name="existing-char"]'
+  ).value;
+  const char_type = document.querySelector(
+    'select[name="existing-char"]'
+  ).value;
+
+  const response = await fetch("/api/character", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/game");
+  } else {
+    alert("Uh Oh Something went wrong");
+  }
+}
 
 document
   .querySelector(".create-char-form")
   .addEventListener("submit", newFormHandler);
+
+document
+  .querySelector(".exist-char-form")
+  .addEventListener("submit", anotherFormHandler);
